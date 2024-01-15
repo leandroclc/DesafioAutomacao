3182b7071556478394a934937592c370eddaca45
Leandro César <leandro.cardoso@fiskaldigital.com.br>
2024-01-15 18:36:08 -0300

    feat: creating repository

---
+++ Application/Repositories/AluraRepository.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entity;
+using Domain.Repositories;
+using System.Text.Json;
+
+namespace Application.Repositories
+{
+    public class AluraRepository : IAluraRepository
+    {
+        public void SaveToTxtFile(DadosCursoAlura dadosCursoAlura)
+        {
+            string filePath = @$"C:\Users\{Environment.UserName}\Downloads\DadosDoCurso.txt";
+            string jsonString = JsonSerializer.Serialize(dadosCursoAlura);
+
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, jsonString);
+            }
+            else
+            {
+                File.AppendAllText(filePath, Environment.NewLine + jsonString);
+            }
+        }
+    }
+}
